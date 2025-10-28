616d1792d0b0f7e0481a2c73d142f74f1c879fe8
Alena3210 <s251740@dtu.dk>
2025-10-28 10:59:33 +0100

    Added substi_T_U function

---
+++ R/substi_T_U.R
@@ -0,0 +1,14 @@
+#' Substitute Thymine with Uracile
+#'
+#' @param substi_T_U a DNA sequence
+#'
+#' @returns a RNA sequence
+#'
+#' @export
+#'
+#' @examples
+#' substi_T_U("ATTCGGGTT")
+substi_T_U <- function(substi_T_U){
+  substi_T_U <- gsub("T", "U", substi_T_U)
+  return(substi_T_U)
+}
